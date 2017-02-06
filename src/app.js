11b08a5d4b5b2daae4d5e93ecc0fb1fa1a2139ba
selfdestruction <vicktor.siryack@gmail.com>
2017-02-06 18:38:03 +0200

    webpack is work

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import moment from 'moment';
 
 let rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(rightNow);
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,46 @@ const webpack = require('webpack');
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-        app: './app.js'
+        app: './app.js',
+        vendor: ['react', 'react-dom', 'rxjs'],
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, './dist/assets'),
         publicPath: '/assets'
     },
-    module: {
-      // rules: [
-      //     {
-      //         test: /\jsx?$/,
-      //     }
-      // ]
-    },
     devServer: {
       contentBase: path.resolve(__dirname, './src')
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlagin(),
-        new HtmlWebpackPlugin({
-            template: 'src/index.tpl.html',
-            inject: 'body',
-            filname: 'index.html'
-        }),
-        new ExtractTextPlugin('[name]-[hash].min.css'),
-
         new webpack.optimize.CommonsChunkPlugin({
             name: 'commons',
             filename: 'common.js',
             minChunks: 2
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015'] }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { modules: true }
+                    },
+                ],
+            },
+
+            // Loaders for other file types can go here
+        ],
+    },
+
 };
